940c242a886ec57d99ad843f79a668295104ca98
Carlos Saavedra <36249113+mastercas1@users.noreply.github.com>
2019-12-23 23:47:31 -0800

    cambio main

--- Views/seeParking.py
+++ Views/seeParking.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 
-class AddTeacher:
+class SeeParking:
   def __init__(self,master,number):
     self.master = master
     self.master.geometry("400x400+200+200")
     self.frame = tk.Frame(self.master)
-    self.label = tk.Label(master,text="Agregar Docente").pack()
+    self.label = tk.Label(master,text="Ver Cajones").pack()
     self.quit = tk.Button(self.frame,text= "Cerrar",command= self.close_window)
     self.quit.pack()
     self.frame.pack()
--- main.py
+++ main.py
@@ -3,29 +3,39 @@ from tkinter.filedialog import askopenfilename
 import Views.addTeacher as at
 import Views.addParking as ap
 import Views.dataTeachers as dt
+import Views.modTeacher as mt
+import Views.deleteTeacher as delt
+import Views.seeParking as sp
+from PIL import ImageTk, Image
+import os
 
 class Main():
 
   def __init__(self,master):
     self.master = master
     self.master.geometry("400x400")
+    self.master.title("Estacionamiento")
     self.frame = tk.Frame(self.master)
+    # img= ImageTk.PhotoImage(Image.open("cimarron.png"))
+    # self.panel(img)
     self.btnCargarArchivo("Cargar Archivo")
     self.btnnew("Agregar Docente","2",at.AddTeacher)
     self.btnnew("Agregar Cajon","3",ap.AddParking)
     self.btnnew("Datos de los Docentes","4",dt.DataTeachers)
+    self.btnnew("Modificar Docente","5",mt.ModTeacher)
+    self.btnnew("Eliminar Docente","6",delt.DeleteTeacher)
+    self.btnnew("Ver Cajones",'7',sp.SeeParking)
     self.frame.pack()
 
   def btnCargarArchivo(self,text):
-    tk.Button(self.frame,text= text,command= lambda: self.callback()).pack() 
+    tk.Button(self.frame,text= text,width= 30,command= lambda: self.callback()).pack(pady=5) 
 
   def btnnew(self,text,number,_class):
-    tk.Button(self.frame,text= text,command= lambda: self.new_window(number,_class)).pack()
+    tk.Button(self.frame,text= text,width= 30,command= lambda: self.new_window(number,_class)).pack(pady=5)
 
   def new_window(self,number,_class):
     self.new = tk.Toplevel(self.master)
     _class(self.new,number)
-
 
   def callback(self):
     # print("click")
@@ -33,15 +43,6 @@ class Main():
     # with open(filename, 'r') as file:
     #   data = file.read()
     # print(data)
-
-  # def principal(self):
-  #   window.geometry("800x500")
-  #   window.title("Sistema de Estacionamiento")
-  #   btnSearchFile = Button(window,text="Cargar Archivo",width=25,command= self.callback)
-  #   btnAddTeacher = Button(window,text="Agregar Docente",command= self.addTeacher)
-  #   btnDeleteTeacher = Button(window, text="Eliminar Docente",command= self.deleteTeacher)
-  #   btnAddBox = Button(window,text="Agregar cajon",command= self.addBox)
-  #   btnTeacherData = Button(window,text="Datos de los docentes",command= self.teacherData)
     
 root = tk.Tk()
 pp = Main(root)
